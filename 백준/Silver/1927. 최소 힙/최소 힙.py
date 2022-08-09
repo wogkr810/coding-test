503b45ae66e65367fa62bbb10cfeb8b798374b93
Jae Hak Lee <jaehahk810@naver.com>
2022-08-09 16:13:42 +0900

    [Silver II] Title: 최소 힙, Time: 160 ms, Memory: 36764 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -0,0 +1,16 @@
+import heapq
+import sys
+
+heap=[]
+
+N=int(sys.stdin.readline())
+
+for i in range(N):
+    tmp=int(sys.stdin.readline())
+    if tmp==0:
+        if len(heap)==0:
+            print(0)
+        else:
+            print(heapq.heappop(heap))
+    else:
+        heapq.heappush(heap,tmp)
