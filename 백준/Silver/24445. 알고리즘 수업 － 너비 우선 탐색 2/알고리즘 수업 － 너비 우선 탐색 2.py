3831008d8015b9916e63f6f66448debbc68488f3
Jae Hak Lee <jaehahk810@naver.com>
2022-08-11 17:15:43 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 656 ms, Memory: 59572 KB -BaekjoonHub

---
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.py
@@ -0,0 +1,30 @@
+from collections import deque
+import sys
+
+N, M, R = map(int,sys.stdin.readline().split())
+G = [[] for _ in range(N+1)]
+for _ in range(M):
+    u, v = map(int, sys.stdin.readline().split())
+    G[u].append(v)
+    G[v].append(u)
+
+for i in range(len(G)):
+    G[i].sort(reverse=True)
+    
+
+visited=[False]*(N+1)
+visited[R]=True
+res=[0]*(N+1)
+queue=deque([R])
+cnt=1
+while queue:
+    tmp=queue.popleft()
+    res[tmp]=cnt
+    cnt+=1
+    for i in G[tmp]:
+        if not visited[i]:
+            queue.append(i)
+            visited[i]=True
+
+for i in res[1:]:
+    print(i)
