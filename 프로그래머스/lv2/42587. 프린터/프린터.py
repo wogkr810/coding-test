ad123d726ba15e076b742019b96cecc1c8762f56
Jae Hak Lee <jaehahk810@naver.com>
2022-08-16 17:15:20 +0900

    Update 프린터.py(add Example)

--- 프로그래머스/lv2/42587. 프린터/프린터.py
+++ 프로그래머스/lv2/42587. 프린터/프린터.py
@@ -1,3 +1,23 @@
+#예시
+'''
+#1번 예시
+priorities
+2 1 3 2 -> 3 2 2 1 -> X 2 2 1 -> X Y 2 1 -> X Y Z 1 -> X Y Z W
+location_list 
+0 1 2 3 -> 2 3 0 1 -> X 3 0 1 -> X Y 0 1 -> X Y Z 1 -> X Y Z W
+(pr , lo) :        (3,2)      (2,3)      (2,0)      (1,1)
+cnt=0               1(답)       2          3          4
+
+#2번 예시
+priorities
+119111->911111->X11111->XY1111->XYZ111->XYZA11->XYZAB1
+location_list 
+012345->234501->X34501->XY4501->XYZ501->XYZA01->XYZAB1
+(pr , lo) :  (9,2)    (1,3)   (1,4)  (1,5)    (1,0)
+cnt=0          1        2       3      4        5(답)
+'''
+
+
 from collections import deque
 
 def solution(priorities, location):
