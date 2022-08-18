2773c3b5d57f920288efda55d077113c2b4c7c82
Jae Hak Lee <jaehahk810@naver.com>
2022-08-18 17:01:48 +0900

    [level 2] Title: [1차] 프렌즈4블록, Time: 3.82 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
+++ 프로그래머스/lv2/17679. ［1차］ 프렌즈4블록/［1차］ 프렌즈4블록.py
@@ -1,52 +1,5 @@
 def solution(m, n, board):
-    ans=board
-    def is_four(arr,x,y,char):
-        if 0<=x<len(arr)-1 and 0<=y<len(arr[0])-1:
-            if char!=0:
-                if arr[x+1][y]==char and arr[x][y+1]==char and arr[x+1][y+1]==char:
-                    return True
-                else:
-                    return False
-
-    def make_minus(hubo,arr):
-        for i in hubo:
-            x,y=i[0],i[1]
-            arr[x][y]=-1
-            arr[x+1][y]=-1
-            arr[x][y+1]=-1
-            arr[x+1][y+1]=-1
-
-    def is_minus(arr):
-        for i in range(len(arr)):
-            for j in range(len(arr[0])):
-                if arr[i][j]==-1:
-                    return False
-        else:
-            return True
-
-    def arr_count(arr):
-        zero_count=0
-        for i in range(len(arr)):
-            for j in range(len(arr[0])):
-                if arr[i][j]==0:
-                    zero_count+=1
-        return zero_count
-    #ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-
-    for i in range(len(ans)):
-        ans[i]=list(ans[i])
-
-    def mat_trans(A):
-        row=len(A)
-        col=len(A[0])    
-
-        B = [[0 for row in range(row)]for col in range(col)]
-
-        for i in range(row):
-            for j in range(col):
-                B[j][i]=A[i][j]
-        return B
-
+    # Transpose로도가능하지만, 전치행렬로 할경우 방향이 반대라 아래가 아닌 위로 쌓임 -> 구글링 코드 참고(https://blackon29.tistory.com/63)
     def rotate_2d(list_2d):
         n = len(list_2d) # 행 길이 계산
         m = len(list_2d[0]) # 열 길이 계산
@@ -56,36 +9,55 @@ def solution(m, n, board):
                 new[j][n-i-1] = list_2d[i][j]
         return new
 
-    ans=rotate_2d(ans)
+    board=rotate_2d(board)
 
-#ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ
-    n=len(ans)
-    m=len(ans[0])
+    # 좌표를 입력 받고, 좌표를 벗어나지않고 위아래대각선이 같으면 Boolean타입
+    def is_four(arr,x,y,char):                            
+        if 0<=x<len(arr)-1 and 0<=y<len(arr[0])-1:
+            if char!=0:                                                                 #밑에서 -1 -> 0으로 치환하기 떄문에 char는 0이 아니어야함
+                if arr[x+1][y]==char and arr[x][y+1]==char and arr[x+1][y+1]==char:
+                    return True                                                         #여기서 -1로 바꿔버리면 겹치는부분해결 불가능
+                else:
+                    return False
 
-    res=n*m
-    while res>0:
-        res-=1
+    # 반복문을 돌며 hubo list와 비교하여, -1로 바꿔줌
+    def make_minus(hubo,arr):                             
+        for i in hubo:
+            x,y=i[0],i[1]
+            arr[x][y]=-1
+            arr[x+1][y]=-1
+            arr[x][y+1]=-1
+            arr[x+1][y+1]=-1
+
+    while True:
         hubo=[]
-        for i in range(len(ans)):
-            for j in range(len(ans[0])):
-                if is_four(ans,i,j,ans[i][j]):
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                if is_four(board,i,j,board[i][j]):
                     hubo.append([i,j])
+        
+        
+        if len(hubo)==0:                                             #처음에는 무지성 n*m했지만, hubo에 들어가지않으면 그냥 그만하면 됨.
+            break
 
-        make_minus(hubo,ans)
+        make_minus(hubo,board)
 
-        ttrs=[]
-        for i in range(len(ans)):
+        ttrs=[]                                                      #블록이 밑으로 내리는게 테트리스같아서..
+        for i in range(len(board)):
             que=[]
             cnt=0
-            for j in range(len(ans[0])):
-                if ans[i][j]!=-1:
-                    que.append(ans[i][j])
+            for j in range(len(board[0])):
+                if board[i][j]!=-1:                                    # -1이 아니면 리스트에 넣어주고
+                    que.append(board[i][j])
                 else:
                     cnt+=1
-            que.extend([0]*cnt)
+            que.extend([0]*cnt)                                      # count만큼 더해주면서 0*cnt로 extend해주기
             ttrs.append(que)
 
-        ans=ttrs.copy()
-
-    return arr_count(ans)
+        board=ttrs.copy()
+    
+    res=0
+    for i in board:
+        res+=i.count(0)
+    return res
 
