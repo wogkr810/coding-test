83fcdd06b31abc6d46aa6ab1a587d399d09ac0ca
Jae Hak Lee <jaehahk810@naver.com>
2022-08-25 19:08:42 +0900

    [Gold II] Title: 반도체 설계, Time: 108 ms, Memory: 36320 KB -BaekjoonHub

---
+++ 백준/Gold/2352. 반도체 설계/반도체 설계.py
@@ -0,0 +1,17 @@
+from bisect import bisect_left
+
+N=int(input())
+arr=list(map(int,input().split()))
+
+dp=[]
+
+for i in range(len(arr)):
+    if len(dp)==0:
+        dp.append(arr[0])
+    elif dp[-1]<=arr[i]:
+        dp.append(arr[i])
+    else:
+        idx=bisect_left(dp,arr[i])
+        dp[idx]=arr[i]
+    # print(dp)
+print(len(dp))
