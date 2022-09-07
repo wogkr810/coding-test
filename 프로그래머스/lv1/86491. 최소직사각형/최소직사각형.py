6d846a9a36f309832994530f5160a038bd8bea80
Jae Hak Lee <jaehahk810@naver.com>
2022-09-07 14:56:56 +0900

    [level 1] Title: 최소직사각형, Time: 5.49 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
+++ 프로그래머스/lv1/86491. 최소직사각형/최소직사각형.py
@@ -1,31 +1,16 @@
-import itertools
-
 def solution(sizes):
-    odd_even_sizes=list(itertools.chain.from_iterable(sizes))
-    max_index=odd_even_sizes.index(max(odd_even_sizes))
+    # sizes_max = max(map(max,sizes))
+    # left_sort = sorted(sizes, reverse = True, key = lambda x : x[0])
+    # right_sort = sorted(sizes, reverse = True, key = lambda x : x[1])
     
-    answer=0
-    tmp=0
-    if (max_index+1)%2==0:
-        for i in range(len(sizes)):
-            if sizes[i][1]>=sizes[i][0]:
-                sizes[i][1]=tmp
-                sizes[i][1]=sizes[i][0]
-                sizes[i][0]=tmp
-        right_index=[]
-        for i in range(len(sizes)):
-            right_index.append(sizes[i][1])
-        answer=max(odd_even_sizes)*max(right_index)
-    else:
-        for i in range(len(sizes)):
-            if sizes[i][0]>=sizes[i][1]:
-                sizes[i][0]=tmp
-                sizes[i][0]=sizes[i][1]
-                sizes[i][1]=tmp
-        left_index=[]
-        for i in range(len(sizes)):
-            left_index.append(sizes[i][0])
-        answer=max(odd_even_sizes)*max(left_index)
     
-
-    return answer
+    for i in range(len(sizes)):
+        if sizes[i][0] >= sizes[i][1]:
+            sizes[i].reverse()
+    
+    l,r = max(list(map(lambda x : x[0], sizes))), max(list(map(lambda x : x[1], sizes)))
+    
+    
+    return l*r
+    
+    # 만약 왼쪽 오른쪽 max가 같다면?
