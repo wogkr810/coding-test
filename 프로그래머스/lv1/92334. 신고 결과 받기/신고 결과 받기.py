810d887cb3c06c238397af2f1e2ad3ab96ebb5f3
Jae Hak Lee <jaehahk810@naver.com>
2022-09-07 12:55:32 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
+++ 프로그래머스/lv1/92334. 신고 결과 받기/신고 결과 받기.py
@@ -4,17 +4,12 @@ def solution(id_list, report, k):
     report = list(set(report))
     reported = defaultdict(int)
     res = [0 for _ in range(len(id_list))]
-    k_report = []
 
     for rpt in report:
         reported[rpt.split()[1]] += 1
 
-    for key, value in reported.items():
-        if value >= k:
-            k_report.append(key)
-
     for rpt in report:
-        if rpt.split()[1] in k_report:
+        if reported[rpt.split()[1]] >= k:
             res[id_list.index(rpt.split()[0])] += 1
 
     return res
