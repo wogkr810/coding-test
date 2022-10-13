0831f1a3c31f95049aed4af364b11d9c44042849
Jae Hak Lee <jaehahk810@naver.com>
2022-10-13 15:59:37 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -0,0 +1,34 @@
+from heapq import heappush, heappop, heapify
+
+def solution(jobs):
+    heap = []
+    for job in jobs:
+        heappush(heap,(job[1],job))
+
+    time = 0
+    res = []
+    tmp_list = []
+
+    while heap:
+        while heap:
+            flag = True
+            tmp = heappop(heap)
+            if tmp[1][0] <= time:
+                time += tmp[1][1]
+                res.append(time-tmp[1][0])
+                flag = False
+                break
+            else:
+                tmp_list.append(tmp)
+        if flag:
+            tmp_list.sort(key=lambda x : (x[1][0],x[1][1]))
+            gt_time = tmp_list.pop(0)
+            time = gt_time[1][0]
+            time += gt_time[1][1]
+            # res.append(time-gt_time[1][0])
+            res.append(gt_time[1][1])
+        for _ in tmp_list:
+            heappush(heap,_)
+        tmp_list = []
+    
+    return int(sum(res)/len(res))
