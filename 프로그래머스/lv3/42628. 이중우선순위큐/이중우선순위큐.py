8279b68d9a730b4fae738d928d151c5aa2ee4ec6
Jae Hak Lee <jaehahk810@naver.com>
2022-10-15 11:13:29 +0900

    [level 3] Title: 이중우선순위큐, Time: 0.04 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
+++ 프로그래머스/lv3/42628. 이중우선순위큐/이중우선순위큐.py
@@ -1,23 +1,50 @@
-from collections import deque
+import heapq
 
 def solution(operations):
-    ans = []
-    for operation in operations:
-        command , num = operation.split()
+    min_heap = []
+    max_heap = [] 
+    visited = [False] * len(operations)
+
+    for i in range(len(operations)):
+        operation, num = operations[i].split()
         num = int(num)
-        if command == "I":
-            ans.append(num)
-            ans = deque(sorted(ans))
-        elif command == "D":
+        if operation == "D":
+            visited[i] = True
+
+        if operation == "I":
+            heapq.heappush(min_heap,(num,i))
+            heapq.heappush(max_heap,(-num,i))
+
+        elif operation == "D":
             try:
                 if num == -1:
-                    ans.popleft()
-                elif num == 1:
-                    ans.pop()
+                    while True:
+                        tmp = heapq.heappop(min_heap)
+                        if not visited[tmp[1]]:
+                            visited[tmp[1]]=True
+                            break
+                else:
+                    while True:
+                        tmp = heapq.heappop(max_heap)
+                        if not visited[tmp[1]]:
+                            visited[tmp[1]]=True
+                            break
             except:
                 pass
 
-    if len(ans) == 0:
+    ans = []
+
+    while min_heap:
+        tmp = min_heap.pop()
+        if not visited[tmp[1]]:
+            ans.append(tmp[0])
+
+    while max_heap:
+        tmp = max_heap.pop()
+        if not visited[tmp[1]]:
+            ans.append(-tmp[0])
+
+    if len(ans) == 0 :
         return [0,0]
     else:
         return [max(ans),min(ans)]
