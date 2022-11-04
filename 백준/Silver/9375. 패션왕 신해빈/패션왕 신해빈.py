fb042edc652f1839e83d1c664d5777569298cd67
Jae Hak Lee <jaehahk810@naver.com>
2022-11-04 14:02:56 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 104 ms, Memory: 32420 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.py
@@ -1,14 +1,13 @@
-from collections import defaultdict,Counter
+from collections import defaultdict  # <- Counter지우기
 T = int(input())
 
 for _ in range(T):
     N = int(input())
     fashion_dict = defaultdict(int)
     for i in range(N):
-        dress, type = input().split()
-        fashion_dict[type] += 1
+        dress, kind = input().split()        # <- type 예약어라 다른 걸로
+        fashion_dict[kind] += 1
     res = 1
     for i in list(fashion_dict.values()):
         res = res *(i+1)
     print(res-1)
-    
