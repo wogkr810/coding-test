47f1c5c89dfe6bac9ed8a5889e4d0767257d6788
Jae Hak Lee <jaehahk810@naver.com>
2022-11-25 12:23:17 +0900

    [Silver III] Title: 두 수의 합, Time: 128 ms, Memory: 42132 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.py
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.py
@@ -1,22 +1,21 @@
 N = int(input())
 arr = list(map(int,input().split()))
-x= int(input())
-
 arr.sort()
+T = int(input())
 
-cnt=0
-start , end = 0, N-1
+cnt = 0
+left = 0
+right = N-1
 
-while start < end :
-    tmp_sum = arr[start] + arr[end]
-    if tmp_sum == x:
-        cnt += 1
-        start += 1
-    
-    elif tmp_sum > x:
-        end -= 1
+while left<right:
+    tmp_sum = arr[left] + arr[right]
+    if tmp_sum == T:
+        cnt +=1
+        left += 1
+    elif tmp_sum > T:
+        right -= 1
+    else:
+        left += 1
 
-    elif tmp_sum < x :
-        start += 1
-        
 print(cnt)
+        
