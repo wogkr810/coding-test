e34ead9eefe818c55adfde0676464ce17d2b530b
Jae Hak Lee <jaehahk810@naver.com>
2023-01-23 17:55:57 +0900

    [level 2] Title: n^2 배열 자르기, Time: 377.78 ms, Memory: 146 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/87390. n＾2 배열 자르기/n＾2 배열 자르기.py
@@ -0,0 +1,17 @@
+def solution(N, left, right):
+    p, q = divmod(left,N)
+    a, b = divmod(right,N)
+    arr = []
+    for i in range(p+1,a+2):
+        arr.extend([i]*i + [j for j in range(i+1,N+1)])
+        
+    cnt = N-b-1
+    while cnt:
+        cnt -= 1
+        arr.pop()
+        
+    return arr[q:]
+
+            
+    # return arr[left:right+1]
+        
