d9ef7b51ad89c9fa06b547525081485f0ef57be1
Jae Hak Lee <jaehahk810@naver.com>
2023-02-07 16:46:52 +0900

    [level 2] Title: [3차] n진수 게임, Time: 249.58 ms, Memory: 10.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.py
@@ -0,0 +1,22 @@
+hex_dict = {"10":'A', "11":"B", "12":"C", "13":"D", "14":"E","15":"F"}
+
+def make_k_jinsu(n,k):
+    res = ''
+    while n > 0:
+        n, div = divmod(n,k)
+        div = str(div)
+        if div in hex_dict:
+            div = hex_dict[div]
+        res += str(div)
+    return res[::-1]
+
+def solution(n, t, m, p):
+    res = '0'
+    for i in range(1,1000*100):
+        res += make_k_jinsu(i,n)
+        
+    ans = ''
+    for i in range(p-1,t*m,m):
+        ans += res[i]
+        
+    return ans
