fc8ab5ed8a5aa29b5d9e1d218cd850cb86900a85
Jae Hak Lee <jaehahk810@naver.com>
2023-02-19 15:04:35 +0900

    [Gold IV] Title: 여행 가자, Time: 56 ms, Memory: 31268 KB -BaekjoonHub

--- 백준/Gold/1976. 여행 가자/여행 가자.py
+++ 백준/Gold/1976. 여행 가자/여행 가자.py
@@ -1,42 +1,28 @@
-from collections import defaultdict
-
 N = int(input())
 M = int(input())
 arr = []
 for _ in range(N):
     arr.append(list(map(int,input().split())))
 plan = list(map(int,input().split()))
+
 plan = [plan[i]-1 for i in range(M)]
 
-graph = defaultdict(list)
-for i in range(N):
-    for j in range(N):
-        if arr[i][j] == 1:
-            graph[i].append(j)
+def dfs(start):
+    if not visited[start]:
+        visited[start] = True
+        for i in range(N):
+            if arr[start][i]:
+                dfs(i)
 
-parent = [i for i in range(N)]
+visited = [False] * N
+dfs(plan[0])
 
-def find_parent(parent,x):
-    if parent[x] != x:
-        parent[x] = find_parent(parent,parent[x])
-    return parent[x]
-
-def union_parent(parent,a,b):
-    a = find_parent(parent,a)
-    b = find_parent(parent,b)
-    if a < b:
-        parent[b] = a
-    else:
-        parent[a] = b
-
-
-for i in range(N):
-    for v in graph[i]:
-        union_parent(parent,i,v)
-
-for j in range(M-1):
-    if parent[plan[j]] != parent[plan[j+1]]:
-        print("NO")
-        break
-else:
+if len(set(plan)) == 1:
     print("YES")
+else:
+    for i in range(len(plan)):
+        if not visited[plan[i]]:
+            print("NO")
+            break
+    else:
+        print("YES")
