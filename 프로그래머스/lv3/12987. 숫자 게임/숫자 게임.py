31759cda901302075b063e0103f27bc8c42d3d88
Jae Hak Lee <jaehahk810@naver.com>
2023-03-07 18:19:22 +0900

    [level 3] Title: 숫자 게임, Time: 40.94 ms, Memory: 18.2 MB -BaekjoonHub

--- 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
+++ 프로그래머스/lv3/12987. 숫자 게임/숫자 게임.py
@@ -1,16 +1,14 @@
+from collections import deque
+
 def solution(A, B):
     A.sort()
     B.sort()
-
-    from collections import deque
     B = deque(B)
 
     cnt = 0
-
     for i in range(len(A)):
         while B:
             tmp = B.popleft()
-
             if tmp > A[i]:
                 cnt += 1
                 break
