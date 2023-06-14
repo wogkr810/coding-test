e38c2185621041edbbede2d895ae3b599fe61616
이재학 | Lee Jaehak <jaehahk810@naver.com>
2023-06-14 13:24:32 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 65.45 ms, Memory: 19.4 MB -BaekjoonHub

--- 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
+++ 프로그래머스/lv2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.py
@@ -8,33 +8,30 @@ def solution(queue1, queue2):
     two_sum = sum(queue2)
     one_len = len(queue1)
     two_len = len(queue2)
-    
-    if (one_sum + two_sum) % 2 != 0:
+
+    if (one_sum + two_sum) % 2 != 0:      # 모든 수의 합이 홀수면 안됨.
         return -1
 
     cnt = 0
-
-    if one_sum == two_sum:
-        return 0
 
     while True:
         if one_sum > two_sum:
             tmp_pop = queue1.popleft()
             queue2.append(tmp_pop)
             one_sum -= tmp_pop
-            two_sum += tmp_pop        
+            two_sum += tmp_pop 
+            cnt += 1       
         elif one_sum < two_sum:
             tmp_pop = queue2.popleft()
             queue1.append(tmp_pop)
             two_sum -= tmp_pop
             one_sum += tmp_pop
-
-        cnt += 1
+            cnt += 1
 
         if one_sum == two_sum:
             break
 
-        if cnt >= 10 *(one_len + two_len):
+        if cnt >= 2 *(one_len + two_len):  # 모두 옮기는과정이 2번 넘으면 그만.
             cnt = -1
             break
     
