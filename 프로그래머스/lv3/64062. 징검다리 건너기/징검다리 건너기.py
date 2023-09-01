0c37c6f2a8e20102f588079afdffe347aef2153c
이재학 | Lee Jaehak <jaehahk810@naver.com>
2023-09-01 12:11:30 +0900

    [level 3] Title: 징검다리 건너기, Time: 209.62 ms, Memory: 18.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv3/64062. 징검다리 건너기/징검다리 건너기.py
@@ -0,0 +1,20 @@
+def solution(stones, k):
+    left, right = 1, 2000000000
+
+    while left <= right:
+        mid = (left + right) // 2
+        cnt = 0
+        for stone in stones:
+            if (stone-mid) <= 0:
+                cnt += 1
+            else:
+                cnt = 0
+
+            if cnt >= k:
+                break
+
+        if cnt >= k:
+            right = mid - 1
+        else:
+            left = mid + 1
+    return left
