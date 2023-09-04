b4a2af4e3fcd2ca8ec0e7b3e0868b54ff75e3c5a
이재학 | Lee Jaehak <jaehahk810@naver.com>
2023-09-04 10:59:16 +0900

    [Gold IV] Title: 트리의 지름, Time: 96 ms, Memory: 36460 KB -BaekjoonHub

---
+++ 백준/Gold/1967. 트리의 지름/트리의 지름.py
@@ -0,0 +1,31 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+graph = [[] for _ in range(N+1)]
+for _ in range(N-1):
+    a, b, c = map(int,input().split())
+    graph[a].append([b,c])
+    graph[b].append([a,c]) 
+
+
+def bfs(start,visited):
+    q = deque([[start,0]])
+    visited[start] = 1
+
+    while q:
+        v, cost = q.popleft()
+        for a,b in graph[v]:
+            if not visited[a]:
+                visited[a] += (b+visited[v])
+                q.append([a,b])
+
+    max_value = max(visited)
+    max_idx = visited.index(max_value)
+    return max_value, max_idx
+
+value, idx = bfs(1,[0] * (N+1))
+ans, idx_2 = bfs(idx, [0] * (N+1))
+
+print(ans-1)
