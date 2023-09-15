fd72e089c79cb4fb3a48c0022c9ec27b4d838f40
이재학 | Lee Jaehak <jaehahk810@naver.com>
2023-09-15 14:11:18 +0900

    [level 2] Title: 줄 서는 방법, Time: 0.02 ms, Memory: 9.97 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/12936. 줄 서는 방법/줄 서는 방법.py
@@ -0,0 +1,25 @@
+from itertools import permutations
+import math
+
+def solution(n, k):
+    num_list = [i for i in range(1,n+1)]
+    res = []
+    while True:
+        a, b = divmod(k,math.factorial(n-1))  
+
+        if b ==0:
+            res.append(num_list[a-1])
+            del num_list[a-1]
+        else:
+            res.append(num_list[a])
+            del num_list[a]
+
+        n -= 1
+        k = b
+
+        if n == 1:
+            break
+
+    res.extend(list(permutations(num_list))[b-1])
+    
+    return res
