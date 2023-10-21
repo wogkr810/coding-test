6d9b462d0631256cac106409e3b2ef7a0b4f3a42
이재학 | Lee Jaehak <jaehahk810@naver.com>
2023-10-21 16:52:18 +0900

    [level 2] Title: 숫자 카드 나누기, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
+++ 프로그래머스/2/135807. 숫자 카드 나누기/숫자 카드 나누기.py
@@ -1,17 +1,38 @@
-def is_divide(arr1, arr2, divisor):
-    for i in arr1:
-        if i % divisor != 0:
-            return False
-    for j in arr2:
-        if j % divisor == 0:
-            return False
-    return True     
-
 def solution(arrayA, arrayB):
-    ans = 0
+    answer = 0
+    if len(arrayA) == 1:
+        if arrayA[0] == arrayB[0]:
+            return 0
+        else:
+            return max(arrayA[0], arrayB[0])
+    else:
+        m1 = 0
+        m2 = 0
+        gcd1 = gcd(arrayA[0], arrayA[1])
+        gcd2 = gcd(arrayB[0], arrayB[1])
+        answer = max(m1,m2)
+        for i in range(2, len(arrayA)):
+            gcd1 = gcd(gcd1, arrayA[i])
+            gcd2 = gcd(gcd2, arrayB[i])
+        if gcd1 != 1 and canDivide(arrayB, gcd1) == False:
+                m1 = gcd1
+        if gcd2 != 1 and canDivide(arrayA, gcd2) == False:
+                m2 = gcd2
+        answer = max(m1,m2)
+    return answer
 
-    for i in range(2,10001):
-        left, right = is_divide(arrayA, arrayB, i), is_divide(arrayB, arrayA, i)
-        if (left and not right) or (right and not left):
-            ans = i
-    return ans
+def gcd(a, b):
+    
+    if b == 0:
+        return a
+    else:
+        if a < b:
+            return gcd(a, b % a)
+        else:
+            return gcd(b, a % b)
+        
+def canDivide(array, a):
+    for i in range(len(array)):
+        if array[i] % a == 0:
+            return True
+    return False
