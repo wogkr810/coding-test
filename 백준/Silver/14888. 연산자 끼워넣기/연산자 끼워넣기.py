87712d2969a00396da45b5a43ee5197c0d10e264
이재학 | Lee Jaehak <jaehahk810@naver.com>
2023-12-02 16:28:45 +0900

    [Silver I] Title: 연산자 끼워넣기, Time: 1216 ms, Memory: 110592 KB -BaekjoonHub

--- 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
+++ 백준/Silver/14888. 연산자 끼워넣기/연산자 끼워넣기.py
@@ -1,30 +1,39 @@
+from itertools import permutations
 import sys
-sys.setrecursionlimit(10000)
+
+input = sys.stdin.readline
 
 N = int(input())
 arr = list(map(int,input().split()))
-operations = list(map(int,input().split()))
+op = list(map(int,input().split()))
 
-add,sub,multi,divide = operations
-min_value = 1e9
-max_value = -1e9
+ops = []
+for i in range(len(op)):
+    ops.extend(str(i+1) * op[i])
 
-def DFS(depth,value,add,sub,multi,divide):
-    global min_value,max_value
-    if depth == N-1:
-        min_value = min(min_value,value)
-        max_value = max(max_value,value)     
-        return   
-    if add:
-        DFS(depth+1,value + arr[depth+1],add-1,sub,multi,divide)
-    if sub:
-        DFS(depth+1,value - arr[depth+1],add,sub-1,multi,divide)
-    if multi:
-        DFS(depth+1,value * arr[depth+1],add,sub,multi-1,divide)
-    if divide:
-        DFS(depth+1,int(value / arr[depth+1]),add,sub,multi,divide-1)
+min_value = int(1e10)
+max_value = int(-1e10)
 
+for perm in permutations(ops):
+    ans = arr[0]
+    for i in range(N-1):
+        if perm[i] == '1':
+            ans += arr[i+1]
+        elif perm[i] == '2':
+            ans -= arr[i+1]
+        elif perm[i] == '3':
+            ans *= arr[i+1]
+        elif perm[i] == '4':
+            if ans >= 0:
+                ans //= arr[i+1]
+            else:
+                ans *= -1
+                ans //= arr[i+1]
+                ans *= -1
 
-DFS(0,arr[0],add,sub,multi,divide)
+    min_value = min(min_value, ans)
+    max_value = max(max_value, ans)
+
 print(max_value)
 print(min_value)
+    
