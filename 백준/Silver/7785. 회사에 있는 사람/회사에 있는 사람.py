bf8d1ccb8e5b22dfccfaa1cdb0c7680e26d73471
이재학 | Lee Jaehak <jaehahk810@naver.com>
2023-12-09 15:24:00 +0900

    [Silver V] Title: 회사에 있는 사람, Time: 4116 ms, Memory: 43124 KB -BaekjoonHub

---
+++ 백준/Silver/7785. 회사에 있는 사람/회사에 있는 사람.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+N = int(input())
+people = defaultdict(str)
+
+for _ in range(N):
+    p, s = input().split()
+    if p in people:
+        if people[p] == 'enter' and s == 'leave':
+            del people[p]
+    else:
+        if s == 'enter':
+            people[p] = 'enter'
+
+for _ in sorted(people.keys(), reverse = True):
+    print(_)
